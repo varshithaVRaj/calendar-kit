d70ab492d9bc302644af9f56c294f8dd8c4ad675
varshitha v <varshitha.vraj@winds.co.in>
2026-06-06 14:54:46 +0530

    add xcode project

---
+++ CalendarKit/CalendarKit/CalendarKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarKitApp.swift
+//  CalendarKit
+//
+//  Created by Varshitha VRaj on 06/06/26.
+//
+
+import SwiftUI
+
+@main
+struct CalendarKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalendarKit/CalendarKit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CalendarKit
+//
+//  Created by Varshitha VRaj on 06/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
